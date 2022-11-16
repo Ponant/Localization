5383a9f5d260d16eeea0ffb1d3881cf17a88c4bb
Ponant <19961806+Ponant@users.noreply.github.com>
2022-11-16 18:53:39 +0100

    Add project files.

---
+++ 6.x/RP/Pages/Language.cshtml.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+
+namespace RP.Pages;
+public class LanguageModel : PageModel
+{
+    private readonly IOptions<RequestLocalizationOptions> _requestLocalizationOptions;
+    public LanguageModel(IOptions<RequestLocalizationOptions> requestLocalizationOptions)
+    {
+        _requestLocalizationOptions = requestLocalizationOptions;
+    }
+
+    [BindProperty(SupportsGet = true)]
+    public string? ReturnUrl { get; set; }
+
+    [BindProperty]
+    public string? Lang { get; set; }
+
+    public void OnGet()
+    {
+    }
+    public IActionResult OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            if (_requestLocalizationOptions.Value.RequestCultureProviders.FirstOrDefault()
+                is CookieRequestCultureProvider cookieRequestCultureProvider)
+            {
+                Response.Cookies.Append(cookieRequestCultureProvider.CookieName,
+                //CookieRequestCultureProvider
+                //.MakeCookieValue(Lang is not null ? new RequestCulture(Lang) :
+                //_requestLocalizationOptions.Value.DefaultRequestCulture),
+                //new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+                CookieRequestCultureProvider
+                        .MakeCookieValue(Lang is not null ? new RequestCulture(Lang) :
+                        _requestLocalizationOptions.Value.DefaultRequestCulture));
+            }
+            return LocalRedirect(ReturnUrl ?? "/");
+        }
+        return LocalRedirect("/");
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Localization
+
+Localization samples and documentation:
+* [ASP.NET 6](6.x)
+* [ASP.NET 7](7.x)
